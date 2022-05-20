30798837c8a95d64e173be762006c983493bcab7
Muhammet Şükrü Kalpaklıoğlu <77413367+mskalpaklioglu@users.noreply.github.com>
2022-05-20 12:44:12 +0300

    Final

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Category } from '../models/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  apiUrl='https://localhost:44394/api/categories/getall'
+  constructor(private httpClient:HttpClient) { }
+
+  getCategories():Observable<ListResponseModel<Category>> {
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
+  }
+
+  isAuthenticated(){
+    if(localStorage.getItem("token")){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+}
